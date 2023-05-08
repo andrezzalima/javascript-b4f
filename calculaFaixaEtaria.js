44804179c8075b12ca711cb58b9e6f460fb4af3a
Andrezza Lima <83400988+andrezzalima@users.noreply.github.com>
2023-05-08 15:17:24 +0100

    Add files via upload

---
+++ calculaFaixaEtaria.js
@@ -0,0 +1,18 @@
+function calculaFaixaEtaria(idade){
+    // escreve aqui a função
+
+    if (idade <= 11){
+        return "Criança"
+    } else if (idade >= 12 && idade <= 17){
+        return "Adolescente"
+    } else if (idade >= 18 && idade <= 64){
+        return "Adulto"
+    } else{
+        return "Sénior"
+    }
+
+
+
+
+
+}
